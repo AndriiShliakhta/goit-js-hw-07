effa15ea5ee38f6c64b2a87571bb8eabd80b2138
Andrew <andrew_shliahta@ukr.net>
2021-07-09 20:23:10 +0300

    tasks 02-04 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,17 @@
 
+const ingredients = [
+		  'Картошка',
+		  'Грибы',
+		  'Чеснок',
+		  'Помидоры',
+		  'Зелень',
+		  'Приправы',
+		];
+
+		const itemsArr = ingredients.map(item=>{
+			const list = document.createElement('li');
+			list.textContent = item;
+			return list;
+		});
+
+		document.querySelector('#ingredients').append(...itemsArr);
--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,13 @@
+const ref = {
+    		counter: document.querySelector('#value'),
+    		decBtn: document.querySelector('button[data-action="decrement"]'),
+    		incBtn: document.querySelector('button[data-action="increment"]')
+    	}
+
+	    function decrement(currentValue) {return  currentValue-=1;}
+	    function increment(currentValue) {return  currentValue+=1;}
+
+    	ref.decBtn.addEventListener('click', () => ref.counter.textContent = decrement(Number(ref.counter.textContent)));
+    	ref.incBtn.addEventListener('click', () => ref.counter.textContent = increment(Number(ref.counter.textContent)));
+
 
