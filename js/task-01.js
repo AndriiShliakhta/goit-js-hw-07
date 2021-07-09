0a835af0a920b5d8520f376ea40d92f472af304d
Andrew <andrew_shliahta@ukr.net>
2021-07-09 20:09:34 +0300

    task-01 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,12 @@
 
+
+    const categories = document.querySelector('#categories');
+    const items = categories.querySelectorAll('.item');
+    console.log(`В списке ${items.length} категории.`);
+
+	items.forEach(item=>{
+		const title = item.querySelector('h2');
+		const list = item.querySelectorAll('ul li');
+		console.log(`Категория: ${title.textContent}`);
+		console.log(`Количество элементов: ${list.length}`);
+	})
