87d7a6f7fa159c722e9a5f1b934988397d86f3e9
Andrew <andrew_shliahta@ukr.net>
2021-07-10 22:13:32 +0300

    task-05-06 done

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,6 @@
+const refs = {
+  input: document.querySelector('#name-input'),
+  output: document.querySelector('#name-output'),
+};
 
+refs.input.addEventListener('input', event => refs.output.textContent = event.currentTarget.value);
--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,10 @@
+const input = document.querySelector('#validation-input');
 
+function checkСondition(event) {
+    if (event.currentTarget.value.length === Number(input.dataset.length)) {
+       return input.style.borderColor = "green";
+    }
+    else return input.style.borderColor = "red";
+}
+
+input.addEventListener('blur', checkСondition);
