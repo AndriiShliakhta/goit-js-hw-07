ad7f7dac94a5029d3c394aadf511852554490b84
Andrew <andrew_shliahta@ukr.net>
2021-07-11 11:16:53 +0300

    task-07 done

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,9 @@
+const ranger = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
 
+ranger.setAttribute('min', "4");
+ranger.setAttribute('max', "100");
+ranger.setAttribute('step', "1");
+
+
+ranger.addEventListener('input', event => text.style.fontSize = event.currentTarget.value + 'px');
